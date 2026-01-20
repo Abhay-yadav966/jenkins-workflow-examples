46cd13f32ce8c9356c7115891867c3e4f54c3c2b
Abhay-yadav966 <yadavabhay966@gmail.com>
2026-01-21 00:25:55 +0530

    All files are added!!

---
+++ src/test/java/com/jenkins/test/Practice/jenkins/PracticeJenkinsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jenkins.test.Practice.jenkins;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PracticeJenkinsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
